b26abfe4e25ae75da0ed02d76cc4aca13e962d60
Ivan Pusic <ivpusic@foi.hr>
2013-09-10 03:31:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pyobjus import autoclass
 
 
 class Ball(Widget):
+    
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
@@ -45,6 +46,7 @@ class PyobjusGame(Widget):
 
 
 class PyobjusBallApp(App):
+    
     def build(self):
         game = PyobjusGame()
         Clock.schedule_interval(game.update, 1.0/60.0)
