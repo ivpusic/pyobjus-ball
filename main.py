1244f4ca143a7964e300a7556182eaf238b0e272
ivpusic <ivpusic@foi.hr>
2013-09-11 03:43:20 +0200

    added example of changing screen brightness with pyobjus. Also updated existing code

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
+from random import random
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
+from kivy.graphics import Color
 from pyobjus import autoclass
-
 
 class Ball(Widget):
 
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
+    h = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
-
 
 class PyobjusGame(Widget):
 
@@ -22,6 +23,7 @@ class PyobjusGame(Widget):
     screen = ObjectProperty(autoclass('UIScreen').mainScreen())
     bridge = ObjectProperty(autoclass('bridge').alloc().init())
     sensitivity = ObjectProperty(50)
+    br_slider = ObjectProperty(None)
 
     def __init__(self, *args, **kwargs):
         super(PyobjusGame, self).__init__()
@@ -31,6 +33,12 @@ class PyobjusGame(Widget):
         self.bridge.stopAccelerometer()
         super(PyobjusGame, self).__dealloc__()
 
+    def reset_ball_pos(self):
+        self.ball.pos = self.width / 2, self.height / 2
+
+    def on_bright_slider_change(self):
+        self.screen.brightness = self.br_slider.value
+
     def update(self, dt):
         self.ball.move()
         self.ball.velocity_x = self.bridge.ac_x * self.sensitivity
@@ -38,9 +46,13 @@ class PyobjusGame(Widget):
 
         if (self.ball.y < 0) or (self.ball.top >= self.height):
             self.ball.velocity_y *= -1
+            self.reset_ball_pos()
+            self.ball.h = random()
 
         if (self.ball.x < 0) or (self.ball.right >= self.width):
             self.ball.velocity_x *= -1
+            self.reset_ball_pos()
+            self.ball.h = random()
 
 
 class PyobjusBallApp(App):
